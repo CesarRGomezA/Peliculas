91dea8e0bc0a292110a8dabc8c090d1a4830ae00
Alumno <cesarrgomez@hotmail.com>
2019-09-18 14:01:00 -0700

    Initial Commit

---
+++ Peliculas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Peliculas
+//
+//  Created by Alumno on 9/18/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
