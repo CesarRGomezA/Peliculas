1f1ca0550fcd6981a2275a4c5cf8a01b39f0ffe0
Alumno <cesarrgomez@hotmail.com>
2019-09-18 14:42:11 -0700

    peliculas

--- Peliculas/ViewController.swift
+++ Peliculas/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    var peliculas : [Pelicula] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        peliculas.append(Pelicula(titulo: "It", director: "Director 1", año: 1992))
+        
+        peliculas.append(Pelicula(titulo: "Siempre hay otro semestre", director: "Director 2", año: 2018))
+        
+        peliculas.append(Pelicula(titulo: "Salbando el semestre ", director: "Director 3", año: 1998))
+    
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peliculas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaPelicula") as? CeldaPeliculaController
+        
+        celda?.lblPelicula.text = peliculas[indexPath.row].titulo
+        celda?.lblDirector.text = peliculas[indexPath.row].director
+        celda?.lblAño.text = "\(peliculas[indexPath.row].año)"
+        
+        
+        return celda!
     }
 
-
+    
 }
 
